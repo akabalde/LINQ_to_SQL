20bd8ab194374575a597c7c468b0a8b6498b7b7c
diegobalde <diegobalderrama93@gmail.com>
2019-10-06 18:43:50 -0300

    Add project files.

---
+++ CA_LINQ_to_SQL_Getting_Started/Program.cs
@@ -0,0 +1,13 @@
+﻿//using System.Data.Linq.DataContext;
+
+namespace CA_LINQ_to_SQL_Getting_Started
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Northwnd nw = new Northwnd(@"northwnd.mdf");
+
+        }
+    }
+}
